d15b75f5364101b0f6ac06596024589e7838f57d
Bmcalkins <b.raders1013@gmail.com>
2020-07-07 17:50:56 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    attr_reader :name
+          
+    def name= (dog_name)
+        @this_dogs_name = dog_name
+    end 
+
+    def name 
+        @this_dogs_name
+    end 
+end
+
+lassie = Dog.new
+lassie.name = "lassie"
+
+puts lassie.name
+
+
+
